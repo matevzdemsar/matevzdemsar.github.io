1c1a657abf101767e5099bb4a89f542f6be53771
matevzdemsar <matevzdemsar1701@gmail.com>
2024-12-24 09:11:06 +0100

    Prepare monsterFilter function for integration into monsters_main

--- monsters/monsterFilter.js
+++ monsters/monsterFilter.js
@@ -9,11 +9,11 @@ import { operatorChoice } from "../motherfunctions.js"
  * @param {{value: int, operation: ">=", "===", "<="}} ac 
  * @param {{value: int, operation: ">=", "===", "<="}} hp 
  * @param {{
- *          walking: {value: number, operation: ">=", "===", "<="},
- *          swimming: {value: number, operation: ">=", "===", "<="},
- *          flying: {value: number, operation: ">=", "===", "<="},
- *          climbing: {value: number, operation: ">=", "===", "<="},
- *          burrowing: {value: number, operation: ">=", "===", "<="}
+ *      walking: {value: number, operation: ">=", "===", "<="},
+ *      swimming: {value: number, operation: ">=", "===", "<="},
+ *      flying: {value: number, operation: ">=", "===", "<="},
+ *      climbing: {value: number, operation: ">=", "===", "<="},
+ *      burrowing: {value: number, operation: ">=", "===", "<="}
  * }} speed - All the speeds must be given, even if they are 0.
  * @param {string[]} vulnerable
  * @param {string[]} resist 
@@ -34,55 +34,52 @@ import { operatorChoice } from "../motherfunctions.js"
 // Add a filter for legendary actions
 
 
-function monsterFilter (
-    monsters,
-    {name = false,
-    type = false,
-    challenge_rating = false,
-    armor_class = false,
-    hit_points = {value: 0, operation: ">="},
-    speed = {walk: {value: 0, operation: ">="},
-            swim: {value: 0, operation: ">="},
-            fly: {value: 0, operation: ">="},
-            climb: {value: 0, operation: ">="},
-            burrow: {value: 0, operation: ">="}} = {},
-    damage_vulnerabilities = [],
-    damage_resistances = [],
-    damage_immunities = [],
-    condition_immunities = [],
-    caster = false,
-    spells = false,
-    legendary_actions = false,
-    str = false,
-    dex = false,
-    con = false,
-    int = false,
-    wis = false,
-    cha = false
+export function monsterFilter (
+  monsters,
+  {name = false,
+  type = false,
+  challenge_rating = false,
+  armor_class = false,
+  hit_points = {value: 0, operation: ">="},
+  speed = {walk: {value: 0, operation: ">="},
+      swim: {value: 0, operation: ">="},
+      fly: {value: 0, operation: ">="},
+      climb: {value: 0, operation: ">="},
+      burrow: {value: 0, operation: ">="}} = {},
+  damage_vulnerabilities = [],
+  damage_resistances = [],
+  damage_immunities = [],
+  condition_immunities = [],
+  caster = false,
+  spells = false,
+  legendary_actions = false,
+  ability_scores = {str: {value: 0, operation: ">="},
+      dex: {value: 0, operation: ">="},
+      con: {value: 0, operation: ">="},
+      int: {value: 0, operation: ">="},
+      wis: {value: 0, operation: ">="},
+      cha: {value: 0, operation: ">="}} = {}
 } = {}) {
 return monsters
-        .filter((m) => !name || m.index.includes(name.toLowerCase().replace("-", " ")))
-        && !type || (m.type === type)
-        && (!challenge_rating || challenge_rating.includes(m.challenge_rating))
-        && (!armor_class || operatorChoice(m.armor_class, armor_class.value, armor_class.operation))
-        && (!hit_points || operatorChoice(m.hit_points, hit_points.value, hit_points.operation))
-        && (Object.keys().every(key =>
-            operatorChoice(m.speed[key].value, speed[key].value, speed[key].operation)))
-        && (!damage_vulnerabilities || damage_vulnerabilities.every((v) =>
-            m.damage_vulnerabilities.includes(v)))
-        && (!damage_resistances || damage_resistances.every((r) =>
-            m.damage_resistances.includes(r)))
-        && (!damage_immunities || damage_immunities.every((i) =>
-            m.damage_immunities.includes(i)))
-        && (!condition_immunities || condition_immunities.every((c) =>
-            m.condition_immunities.includes(c)))
-        && (!caster || (m.caster === caster))
-        && (!legendary_actions || (m.legendary_actions === legendary_actions))
-        && (!legendary_resistances || (m.legendary_resistances === legendary_resistances))
-        && (!str || operatorChoice(m.str, str.value, str.operation))
-        && (!dex || operatorChoice(m.dex, dex.value, dex.operation))
-        && (!con || operatorChoice(m.con, con.value, con.operation))
-        && (!int || operatorChoice(m.int, int.value, int.operation))
-        && (!wis || operatorChoice(m.wis, wis.value, wis.operation))
-        && (!cha || operatorChoice(m.cha, cha.value, cha.operation));
+    .filter((m) => !name || m.index.includes(name.toLowerCase().replace("-", " ")))
+    && !type || (m.type === type)
+    && (!challenge_rating || challenge_rating.includes(m.challenge_rating))
+    && (!armor_class || operatorChoice(m.armor_class, armor_class.value, armor_class.operation))
+    && (!hit_points || operatorChoice(m.hit_points, hit_points.value, hit_points.operation))
+    && (Object.keys(speed).every((key) =>
+      operatorChoice(m.speed[key].value, speed[key].value, speed[key].operation)))
+    && (!damage_vulnerabilities || damage_vulnerabilities.every((v) =>
+      m.damage_vulnerabilities.includes(v)))
+    && (!damage_resistances || damage_resistances.every((r) =>
+      m.damage_resistances.includes(r)))
+    && (!damage_immunities || damage_immunities.every((i) =>
+      m.damage_immunities.includes(i)))
+    && (!condition_immunities || condition_immunities.every((c) =>
+      m.condition_immunities.includes(c)))
+    && (!caster || (m.caster === caster))
+    && (!legendary_actions || (m.legendary_actions === legendary_actions))
+    && (!legendary_resistances || (m.legendary_resistances === legendary_resistances))
+    && (Object.keys(ability_scores).every((key) =>
+      operatorChoice(m.ability_scores[key].value, ability_scores[key].value,
+        ability_scores[key].operation)));
 }
